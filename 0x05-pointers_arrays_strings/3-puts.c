88db22b13d11f9f2955b4e2bca32a9466a1ec2b9
gkusikwarteng <gilbert.kusi1512@gmail.com>
2023-10-16 22:17:44 -0700

    3-puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts - function to print a string and a new line
+ * @str: parameter
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for(i = 0; str[i] != '\0'; i++)
+		_putchar (str[i]);
+	_putchar ('\n');
+}
