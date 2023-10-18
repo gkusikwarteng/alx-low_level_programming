618bc8cd5ddb5ec5f42df1f97ba4e821e1098f9e
gkusikwarteng <gilbert.kusi1512@gmail.com>
2023-10-17 22:24:28 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * puts2 - main function
+ * @str: parameter for the function
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
