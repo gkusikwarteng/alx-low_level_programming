1a3aab4981676916ae484c13a95705d1d964ccd8
gkusikwarteng <gilbert.kusi1512@gmail.com>
2023-11-08 21:17:27 -0800

    calc

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -9,10 +9,10 @@
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-		{"+". op_add},
-		{"-". op_sub},
-		{"*". op_mul},
-		{"/". op_div},
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
 		{"%", op_mod},
 		{NULL, NULL}
 	};
@@ -21,5 +21,5 @@ int (*get_op_func(char *s))(int, int)
 	while (ops[i].op != NULL && *(ops[i].op) != *s)
 		i++;
 
-	return (ops[i],f);
+	return (ops[i].f);
 }
